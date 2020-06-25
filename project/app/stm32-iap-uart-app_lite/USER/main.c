3b7d2f702cd36ca1c714040a4eb4a692b2785131
qitas <qitas@qitas.cn>
2020-06-25 09:11:41 +0800

    nothing

--- project/app/stm32-iap-uart-app_lite/USER/main.c
+++ project/app/stm32-iap-uart-app_lite/USER/main.c
@@ -6,21 +6,22 @@
 
 int main(void)
 {
-	u16 times=0;  
+	u16 times=0;
 	IAP_Init();
-	delay_init(72);	    
+	delay_init(72);
 	NVIC_Configuration();
-	uart_init(115200);  
- 	LED_Init();	       
+	uart_init(115200);
+	LED_Init();
 	while(1)
 	{
 		IAP_Handle(&USART_RX_STA,USART_RX_BUF);
 		times++;
-		delay_ms(10);  
-		if(times%50==0){
+		delay_ms(10);
+		if(times%50==0)
+		{
 			LED0=!LED0;			//闪烁LED(PC13),提示系统正在运行.
 			printf("qitas test app lite\r\n");
 		}
-	}	 
+	}
 }
 
