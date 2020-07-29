5682e64c1d9cb3bc821b305718205d2fc88e8cbc
qitas <qitas@qitas.cn>
2020-07-29 19:59:55 +0800

    fix some thing

--- project/app/stm32-iap-uart-app_lite/Binary/hextobin.bat
+++
@@ -1,7 +0,0 @@
-if exist .\..\*.hex (.\..\Binary\hex2bin.exe  .\..\OUTPUT\*.hex)
-if exist .\..\*.hex (.\..\Binary\hex2bin.exe  .\..\OUTPUT\*.hex)
-if exist .\..\*.hex (.\..\Binary\hex2bin.exe  .\..\OUTPUT\*.hex)
-if exist .\..\*.hex (.\..\Binary\hex2bin.exe  .\..\OUTPUT\*.hex)
-if exist .\..\*.hex (.\..\Binary\hex2bin.exe  .\..\OUTPUT\*.hex)
-if exist .\..\*.hex (.\..\Binary\hex2bin.exe  .\..\OUTPUT\*.hex)
-exit
