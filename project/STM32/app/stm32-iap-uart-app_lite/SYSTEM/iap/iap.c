32e9c9152284dd157e421e343e2b7c7f624a7581
qitas <qitas@qitas.cn>
2020-07-29 10:54:33 +0800

    nothing

--- project/STM32/app/stm32-iap-uart-app_lite/SYSTEM/iap/iap.c
+++ project/STM32/app/stm32-iap-uart-app_lite/SYSTEM/iap/iap.c
@@ -1,42 +1,74 @@
-#include "iap.h"
-#include "stmflash.h"
+/******************************************************************************
+****版本：1.0.0
+****平台：
+****日期：2020-07-29
+****作者：Qitas
+****版权：
+*******************************************************************************/
+
 #include <string.h>
-/************************************************************************/
+#include "stmflash.h"
+#include "iap.h"
+
+/*******************************************************************************
+**函数信息 ：
+**功能描述 ：
+**输入参数 ：无
+**输出参数 ：无
+*******************************************************************************/
 void IAP_WriteFlag(uint16_t flag)
 {
 #if (USE_BKP_SAVE_FLAG == 1)
 	PWR->CR |= PWR_CR_DBP;
 	BKP_WriteBackupRegister(IAP_FLAG_ADDR, flag);
 	PWR->CR &= ~PWR_CR_DBP;
-#else 
+#else
 	FLASH_Unlock();
 	STMFLASH_Write(IAP_FLAG_ADDR, &flag, 1);
 	FLASH_Lock();
-#endif	
+#endif
 }
 
-/************************************************************************/
+/*******************************************************************************
+**函数信息 ：
+**功能描述 ：
+**输入参数 ：无
+**输出参数 ：无
+*******************************************************************************
 uint16_t IAP_ReadFlag(void)
 {
 #if (USE_BKP_SAVE_FLAG == 1)
 	return BKP_ReadBackupRegister(IAP_FLAG_ADDR);
 #else
-	return STMFLASH_ReadHalfWord(IAP_FLAG_ADDR);  
-#endif	
+	return STMFLASH_ReadHalfWord(IAP_FLAG_ADDR);
+#endif
 }
 
+/***************************************************************************************
+**函数信息 ：
+**功能描述 ：
+**输入参数 ：无
+**输出参数 ：无
+****************************************************************************************/
 void IAP_Init(void)
 {
 #if (USE_BKP_SAVE_FLAG == 1)
-	RCC_APB1PeriphClockCmd(RCC_APB1ENR_PWREN | RCC_APB1ENR_BKPEN , ENABLE); 
+	RCC->APB1ENR |= (RCC_APB1ENR_PWREN | RCC_APB1ENR_BKPEN);
+//	RCC_APB1PeriphClockCmd(RCC_APB1ENR_PWREN | RCC_APB1ENR_BKPEN , ENABLE);
 #endif
-	NVIC_SetVectorTable(STM32_FLASH_BASE, IAP_FLASH_SIZE);//设置中断向量表
+	SCB->VTOR = STM32_FLASH_BASE | IAP_FLASH_SIZE;
+//	NVIC_SetVectorTable(STM32_FLASH_BASE, IAP_FLASH_SIZE);
 }
-
+/***************************************************************************************
+**函数信息 ：
+**功能描述 ：
+**输入参数 ：无
+**输出参数 ：无
+****************************************************************************************/
 void IAP_Handle(u8 *mark,u8 * cmd)
 {
 	if(mark[0]&0x80)
-	{					 
+	{
 		mark[0]=0;
 		if(strcmp((char *)cmd, "update") == 0)
 		{
@@ -46,22 +78,23 @@ void IAP_Handle(u8 *mark,u8 * cmd)
 		else if(strcmp((char *)cmd, "erase") == 0)
 		{
 			IAP_WriteFlag(ERASE_FLAG_DATA);
-			NVIC_SystemReset();		
+			NVIC_SystemReset();
 		}
 		else if(strcmp((char *)cmd, "menu") == 0)
 		{
 			IAP_WriteFlag(INIT_FLAG_DATA);
-			NVIC_SystemReset();	
+			NVIC_SystemReset();
 		}
 		else if(strcmp((char *)cmd, "runapp") == 0)//reset
 		{
-			NVIC_SystemReset();	
+			NVIC_SystemReset();
 		}
 		else
 		{
 			printf("指令有误\r\n");
-			NVIC_SystemReset();	
+			NVIC_SystemReset();
 		}
 	}
 }
 
+/*-------------------------(C) COPYRIGHT 2020 QITAS --------------------------*/
--- project/STM32/app/stm32-iap-uart-app_lite/USER/main.c
+++ project/STM32/app/stm32-iap-uart-app_lite/USER/main.c
@@ -1,3 +1,11 @@
+/******************************************************************************
+****版本：1.0.0
+****平台：
+****日期：2020-07-29
+****作者：Qitas
+****版权：
+*******************************************************************************/
+
 #include "led.h"
 #include "delay.h"
 #include "sys.h"
@@ -6,21 +14,22 @@
 
 int main(void)
 {
-	u16 times=0;  
+	u16 times=0;
 	IAP_Init();
-	delay_init(72);	    
-	NVIC_Configuration();
-	uart_init(115200);  
- 	LED_Init();	       
+	delay_init(72);
+	// NVIC_Configuration();
+	uart_init(115200);
+	LED_Init();
 	while(1)
 	{
-		IAP_Handle(&USART_RX_STA,USART_RX_BUF);
+		// IAP_Handle(&USART_RX_STA,USART_RX_BUF);
 		times++;
-		delay_ms(10);  
+		delay_ms(10);
 		if(times%30==0){
 			LED0=!LED0;//闪烁LED(PC13),提示系统正在运行.
 			printf("qitas test\r\n");
 		}
-	}	 
+	}
 }
 
+/*-------------------------(C) COPYRIGHT 2020 QITAS --------------------------*/
